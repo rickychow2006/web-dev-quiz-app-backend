b36153ab85af3612fd925ed41ab01f00146daf34
rickychow2006 <rickychow0716@gmail.com>
2021-11-04 16:12:49 -0500

    added routing for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do 
+		resources :users, only: [:index, :create, :show, :update]
+	end 
+
+  namespace :api do 
+    namespace :v1 do
+      resources :categories, only: [:index] do 
+        resources :questions, only: [:index]
+      end 
+    end 
+  end 
+  
 end
