81d56c99da1b800a86116b320785b8da11c6f41d
rickychow2006 <rickychow0716@gmail.com>
2021-12-09 17:20:07 -0600

    completed User db

--- db/migrate/20211104211040_create_users.rb
+++ db/migrate/20211104211040_create_users.rb
@@ -1,14 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.text :question 
-      t.string :choice1
-      t.string :choice2
-      t.string :choice3
-      t.string :choice4
-      t.string :answer 
-      t.integer :category_id 
+      t.text :name 
 
+      t.integer :data_structure_score, default: 0
+      t.integer :computer_science_score, default: 0
+      t.integer :javascript_score, default: 0
+      t.integer :modern_react_score, default: 0
       t.timestamps
     end
   end
