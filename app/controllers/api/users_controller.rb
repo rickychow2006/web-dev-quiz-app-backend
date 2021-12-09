a83051b8a4d058e3440688f08c798a027b1994f7
rickychow2006 <rickychow0716@gmail.com>
2021-12-09 14:11:19 -0600

    completed user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,44 @@
 class Api::UsersController < ApplicationController
-end
+    def index
+        users = User.all
+        render json:UserSerializer.new(users)
+    end
+
+    def create 
+        user = User.create(user_params)
+
+        if user.save
+            render json:UserSerializer.new(user), status:accepted
+        else 
+            render json:{errors: users.errors.full_messages}, status: :unprocessable_entity
+        end 
+
+    def show 
+        user = User.find_by(id: params[:id])
+
+        if user 
+            render json:user
+        else 
+            render json: {message: 'User not found.'}
+        end 
+    end 
+
+    def update
+        user = User.find_by(id: params[:id])
+        user.update(user_params)
+
+        if user.save
+            render json: user
+        else 
+            render json: {message: `User not saved.`}
+        end 
+    end 
+
+    private 
+
+        def user_params
+            params.require(:user).permit(:name, :data_structure, :computer_science, :javascript, :modern_react)
+        end 
+
+
+end 
