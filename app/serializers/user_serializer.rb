137d8acaf59836856777840bc302a565eb14c178
rickychow2006 <rickychow0716@gmail.com>
2021-12-09 22:40:05 -0600

    completed user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :id, :name
+    attributes :id, :name, :data_structure_score, :computer_science_score, :javascript_score, :modern_react_score
   end
